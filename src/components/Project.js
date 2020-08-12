1bfe1d42a40b7c06a54bc255221b625414252b3d
BCT <bertrand.chevalier@gmail.com>
2020-08-12 19:36:06 -0400

    Removed link from all project card and limited it to the image and the title

--- src/components/Project.js
+++ src/components/Project.js
@@ -21,44 +21,43 @@ class Project extends Component {
         
         return (
             <div className="project">
-                <a 
-                href={this.props.link} 
-                target="_blank"
-                rel="noopener noreferrer">
+                
+                <div className="project_header">
+                    <h3 className="project_name">
+                        <a href={this.props.link} 
+                        target="_blank"
+                        rel="noopener noreferrer">
+                            {this.props.name}
+                        </a>
+                    </h3>
+                </div>
 
-                    <div className="project_header">
-                        <h3 className="project_name">
-                            <a href={this.props.link} 
-                            target="_blank"
-                            rel="noopener noreferrer">
-                                {this.props.name}
-                            </a>
-                        </h3>
-                    </div>
-
-                    <div className="project_body">
+                <div className="project_body">
+                    <a 
+                    href={this.props.link} 
+                    target="_blank"
+                    rel="noopener noreferrer">
                         <img
-                            className="project_image"
-                            width='250px'
-                            height='250px'
-                            src={this.props.image}
-                            alt=''
-                        ></img>
+                        className="project_image"
+                        width='250px'
+                        height='250px'
+                        src={this.props.image}
+                        alt=''></img>
+                    </a>
 
-                        <div className="project_card">
-                            <div className="project_description">
-                                {this.props.description}
-                            </div>
-
-                            <div className="project_technology">
-                                <ul>
-                                    {this.renderTechnology()}
-                                </ul>
-                            </div>
+                    <div className="project_card">
+                        <div className="project_description">
+                            {this.props.description}
                         </div>
-                        
+
+                        <div className="project_technology">
+                            <ul>
+                                {this.renderTechnology()}
+                            </ul>
+                        </div>
                     </div>
-                </a>
+
+                </div>
             </div>
         )
     }
