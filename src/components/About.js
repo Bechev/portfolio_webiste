f92d3d9ae26c78b63e5a8b891033559ff8d56602
BCT <bertrand.chevalier@gmail.com>
2020-08-12 22:19:00 -0400

    Modified and fixed typos in About description

--- src/components/About.js
+++ src/components/About.js
@@ -20,9 +20,9 @@ class About extends Component {
                             I have been interested in programming since high school learning the basics of programming with online resource (and that's before Youtube existed). 
                             While studying Finance in college, I soon became the "coder" in all group projects when it was not yet cool.
                             <br></br>
-                            Fast forward to 2015, I moved to the US and discovered the concept of bootcamps. I was sold and attended the Flatiron Online Web Developer program.
+                            Fast forward ~5 years, I moved to the US and discovered the concept of bootcamps. I was sold and attended the Flatiron Online Web Developer program.
                             <br></br>
-                            Since then I oriented my career towards coding, becoming the "Mr Automation" of my company. I took an got an internal data science certification using R and Pyton.
+                            Since then I oriented my career towards coding. I became the "Mr Automation" of my company and obtaind an internal data science certification using R and Pyton within.
                             <br></br>
                             On the side, I've been focusing on building projects and deepening my knowledge of programming and exploring new technologies 
                         </span>
