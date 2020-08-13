80b13227682547ace52ea782795f797181a373f2
BCT <bertrand.chevalier@gmail.com>
2020-08-12 22:14:36 -0400

    Added a project and changed descriptions

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
 import Project from './Project.js'
 import image_placeholder from '../images/image_placeholder.jpg'
-import blackjack_screenshot from '../images/blackjack_screenshot.png'
+import simmering_screenshot from '../images/simmering_screenshot.png'
 import losertictactoe_screenshot from '../images/losertictactoe_screenshot.png'
+import blackjack_screenshot from '../images/blackjack_screenshot.png'
 import onedayonepun_screenshot from '../images/1day1pun_screenshot.png'
+import pythonblackjack_screenshot from '../images/pythonblackjack_screenshot.png'
 import './components.css'
 
 
 
 class Portfolio extends Component {
-
-    // constructor(props) {
-    //     super(props);
-    //     }
-    // }
 
     render() {
 
@@ -21,26 +18,33 @@ class Portfolio extends Component {
             <div className="portfolio">
                 <h2>Some stuff I built</h2>
                 <Project 
-                    link="https://simmering.herokuapp.com/" 
-                    name="Simmering"
-                    image={image_placeholder} 
+                    link="http://bertrandchevalier.com/Simmering%20demo.mp4" 
+                    name="Meal Planning WebApp"
+                    image={simmering_screenshot}
                     description="A website that changes the way you do your groceries by making it easy to eat seasonal and local food while reducing waste"
-                    technology={[ "React-Redux", "Ruby on Rails", "Devise-token-auth"]}/>
+                    technology={[ "React-Redux", "Ruby on Rails", "SQLite database", "Selenium scripts to scrap websites to populate database"]}/>
 
-                <Project 
-                    link="http://bertrandchevalier.com/losertictactoe/index.html" 
-                    name="Losing Tic Tac Toe"
-                    image={losertictactoe_screenshot} 
-                    description="This project makes Tic Tac Toe interesting again by changing the game's objective. You're X, the AI is O, try to lose, good luck."
-                    technology={[ "HTML", "Vanilla Javascript", "Minimax Algorithm"]}/>
-                
                 <Project 
                     link="https://lightnino.herokuapp.com/" 
                     name="Online Black Jack"
                     image={blackjack_screenshot} 
                     description="When gambling is illegal, you can always play Black Jack here. And you won't lose money. Won't make any either though..."
-                    technology={[ "React", "Ruby on Rails", "Postgres"]}/>
+                    technology={[ "React", "Ruby on Rails", "Postgres database", "ActiveRecord"]}/>
 
+                <Project 
+                    link="http://bertrandchevalier.com/losingtictactoe/index.html" 
+                    name="Losing Tic Tac Toe"
+                    image={losertictactoe_screenshot} 
+                    description="This project makes Tic Tac Toe interesting again by changing the game's objective. You're X, the AI is O, try to lose, good luck."
+                    technology={[ "HTML", "Vanilla Javascript", "Minimax Algorithm"]}/>
+
+                <Project 
+                    link="https://bertrandchevalier.com/Python%20blackjack%20demo.mp4" 
+                    name="Python CLI BlackJack"
+                    image={pythonblackjack_screenshot} 
+                    description="Do I like too much developping Black Jack games? Probably, but this is in Python and CLI only. So it changes a bit. No?"
+                    technology={[ "Python", "Pytest", "Python Error handling"]}/>
+                
                 <Project 
                     link="https://twitter.com/1day1pun" 
                     name="One day, one pun"
