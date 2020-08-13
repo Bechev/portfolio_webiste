6f42452f093f7b13b59638b0a1fc51aaaff1642c
BCT <bertrand.chevalier@gmail.com>
2020-08-12 20:53:54 -0400

    Updated the About section of the website

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Portfolio from './components/Portfolio.js'
 import './App.css';
 
 function App() {
+  topFunction(){
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+
   return (
     <div className="App">
       <div className="background">
@@ -16,6 +21,7 @@ function App() {
         <About/>
         <br></br>
         <Portfolio/>
+        <button onclick={this.topFunction()} id="myBtn" title="Go to top">Top</button>
       </div>
     </div>
   );
--- src/components/About.js
+++ src/components/About.js
@@ -17,13 +17,13 @@ class About extends Component {
                 <div class='text'>
                     <p>
                         <span>
-                            {/* I started my career in France as a Strategy and Operations consultant in the Financial industry. */}
                             I have been interested in programming since high school learning the basics of programming with online resource (and that's before Youtube existed). 
                             While studying Finance in college, I soon became the "coder" in all group projects when it was not yet cool.
                             <br></br>
                             Fast forward to 2015, I moved to the US and discovered the concept of bootcamps. I was sold and attended the Flatiron Online Web Developer program.
                             <br></br>
-                            Since then I oriented my career towards coding, becoming the "Mr Automation" of my company. 
+                            Since then I oriented my career towards coding, becoming the "Mr Automation" of my company. I took an got an internal data science certification using R and Pyton.
+                            <br></br>
                             On the side, I've been focusing on building projects and deepening my knowledge of programming and exploring new technologies 
                         </span>
                         <span> </span>
